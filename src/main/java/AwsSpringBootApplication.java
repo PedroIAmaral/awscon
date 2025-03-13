cea1de63413835bd6543baa9cb952f2e0e59b47d
PedroIAmaral <pedrobrandao.amaral@gmail.com>
2025-03-13 00:30:08 -0400

    primeiro commit

---
+++ src/main/java/AwsSpringBootApplication.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.core.client.config.ClientOverrideConfiguration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@SpringBootApplication
+public class AwsSpringBootApplication {
+
+
+
+}
